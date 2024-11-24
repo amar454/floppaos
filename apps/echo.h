72f17738b400e8f85629f9664d354f53b53037ab
amar454 <aaamargml@gmail.com>
2024-11-23 16:16:10 -0800

    remove echo obj

--- apps/echo.h
+++ apps/echo.h
@@ -8,5 +8,5 @@ extern unsigned int vga_index; // Declare as extern
 void put_char(char c, unsigned char color); // Function to display a character
 void read_input(char *buffer, int max_len, unsigned char color); // Function to read input from the user
 void echo(const char *str, unsigned char color); // Function to echo a string with color
-
+void echo_address(void *ptr, unsigned char color);
 #endif // ECHO_H
