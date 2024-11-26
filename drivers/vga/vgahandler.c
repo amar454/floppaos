9928276572dbcfe1aa84e4c3ed8f90bde30e3c16
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2024-11-26 08:40:06 -0800

    Update vgahandler.c

--- drivers/vga/vgahandler.c
+++ drivers/vga/vgahandler.c
@@ -1,3 +1,25 @@
+/*
+Copyright 2024 Amar Djulovic <aaamargml@gmail.com>
+
+This file is part of FloppaOS.
+
+FloppaOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+FloppaOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with FloppaOS. If not, see <https://www.gnu.org/licenses/>.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+vgahandler.c
+
+This is the VGA text and graphics handler for FloppaOS. Text output is completely safe and implemented; however, the graphics are very much in a beginning state of development.
+
+Therefore, graphics are not yet accessable to the end user until it can be implemented safely, which is a top priority of development for me right now.
+
+Good luck reading half of this. 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 #include "vgahandler.h"
 #include "../../lib/flopmath.h"
 
