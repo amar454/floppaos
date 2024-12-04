262c36bae7cd1f5026eba3b36bc2963f187e8f18
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2024-12-04 12:59:46 -0800

    Update str.h

--- lib/str.h
+++ lib/str.h
@@ -1,9 +1,24 @@
+/*
+str.h - string functions for floppaOS
+
+Copyright 2024 Amar Djulovic <aaamargml@gmail.com>
+
+This file is part of FloppaOS.
+
+FloppaOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+FloppaOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with FloppaOS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef STR_H
 #define STR_H
 
-#include <stddef.h> // For size_t
+#include <stdarg.h>
+#include <stddef.h>  // For size_t
 
-// String manipulation functions
+// String functions
 void flopstrcopy(char *dst, const char *src, size_t len);
 int flopatoi(const char *str);
 size_t flopstrlen(const char *str);
@@ -16,9 +31,6 @@ char *flopstrstr(const char *haystack, const char *needle);
 char *flopstrrchr(const char *str, int c);
 char *flopstrtok(char *str, const char *delim);
 char *flopstrchr(const char *str, int c);
-int flopitoa(int value, char *buffer, int width);
-int flopvsnprintf(char *buffer, size_t size, const char *format, va_list args);
-int flopsnprintf(char *buffer, size_t size, const char *format, ...);
 
 // Random number generation
 unsigned int floprand(void);
@@ -26,5 +38,13 @@ void flopsrand(unsigned int seed);
 
 // Time function
 unsigned int floptime(void);
+
+// Integer-to-string conversion
+int flopitoa(int value, char *buffer, int width);
+int flopitoa_hex(unsigned int value, char *buffer, int width, int is_upper);
+
+// Formatted output
+int flopvsnprintf(char *buffer, size_t size, const char *format, va_list args);
+int flopsnprintf(char *buffer, size_t size, const char *format, ...);
 
 #endif // STR_H
