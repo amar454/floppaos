eecc027a09455df1b2d2c1536d5969ea9556fbb4
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2024-12-04 12:43:01 -0800

    Update str.h

--- lib/str.h
+++ lib/str.h
@@ -2,18 +2,29 @@
 #define STR_H
 
 #include <stddef.h> // For size_t
-#include <stdarg.h> // for va_list
-// Function declarations
+
+// String manipulation functions
 void flopstrcopy(char *dst, const char *src, size_t len);
+int flopatoi(const char *str);
 size_t flopstrlen(const char *str);
 int flopstrcmp(const char *s1, const char *s2);
 int flopstrncmp(const char *s1, const char *s2, size_t n);
+void flopstrrev(char *str);
+void flopstrncpy(char *dst, const char *src, size_t n);
+void flopstrcat(char *dst, const char *src);
+char *flopstrstr(const char *haystack, const char *needle);
+char *flopstrrchr(const char *str, int c);
+char *flopstrtok(char *str, const char *delim);
+char *flopstrchr(const char *str, int c);
+int flopitoa(int value, char *buffer, int width);
+int flopvsnprintf(char *buffer, size_t size, const char *format, va_list args);
+int flopsnprintf(char *buffer, size_t size, const char *format, ...);
+
+// Random number generation
 unsigned int floprand(void);
 void flopsrand(unsigned int seed);
-unsigned int floptime(void); // New function declaration for time
-char *flopstrtok(char *str, const char *delim); // Declaration for flopstrtok
-char *flopstrchr(const char *str, int c);
-int flopvsnprintf(char *buf, size_t size, const char *format, va_list args);
-int flopsnprintf(char *buf, size_t size, const char *format, ...);
-int flopatoi(const char *str) ;
+
+// Time function
+unsigned int floptime(void);
+
 #endif // STR_H
