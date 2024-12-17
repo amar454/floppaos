b9da41929884ea9dd3c047ff109e5a3df0726629
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2024-12-17 11:48:50 -0800

    Update str.h

--- lib/str.h
+++ lib/str.h
@@ -18,33 +18,47 @@ You should have received a copy of the GNU General Public License along with Flo
 #include <stdarg.h>
 #include <stddef.h>  // For size_t
 
-// String functions
+// Function declarations
 void flopstrcopy(char *dst, const char *src, size_t len);
+size_t flopstrlcpy(char *dst, const char *src, size_t size);
 int flopatoi(const char *str);
 size_t flopstrlen(const char *str);
+size_t flopstrnlen(const char *str, size_t maxlen);
 int flopstrcmp(const char *s1, const char *s2);
 int flopstrncmp(const char *s1, const char *s2, size_t n);
 void flopstrrev(char *str);
 void flopstrncpy(char *dst, const char *src, size_t n);
 void flopstrcat(char *dst, const char *src);
+size_t flopstrlcat(char *dst, const char *src, size_t size);
+char *flopstrtrim(char *str);
+char *flopstrreplace(char *str, const char *old, const char *new_str);
+char **flopstrsplit(const char *str, const char *delim);
+void flopstrreverse_words(char *str);
 char *flopstrstr(const char *haystack, const char *needle);
 char *flopstrrchr(const char *str, int c);
-char *flopstrtok(char *str, const char *delim);
-char *flopstrchr(const char *str, int c);
 
-// Random number generation
+// Random number generation functions
 unsigned int floprand(void);
 void flopsrand(unsigned int seed);
 
-// Time function
+// Simple time function
 unsigned int floptime(void);
 
-// Integer-to-string conversion
+// Tokenization functions
+char *flopstrtok(char *str, const char *delim);
+char *flopstrtok_r(char *str, const char *delim, char **saveptr);
+
+// Memory functions
+char *flopstrdup(const char *str);
+
+// Character search functions
+char *flopstrchr(const char *str, int c);
+
+// Integer to string conversion functions
 int flopitoa(int value, char *buffer, int width);
 int flopitoa_hex(unsigned int value, char *buffer, int width, int is_upper);
 
-// Formatted output
+// Formatted output functions
 int flopvsnprintf(char *buffer, size_t size, const char *format, va_list args);
 int flopsnprintf(char *buffer, size_t size, const char *format, ...);
-
 #endif // STR_H
