a0e49ae22802b64c784dc1bc34558cb30537d535
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2024-12-17 19:15:10 -0800

    Update flopmath.c

--- lib/flopmath.c
+++ lib/flopmath.c
@@ -1,3 +1,17 @@
+/*
+flopmath.c - mathematical functions for floppaOS
+
+Copyright 2024 Amar Djulovic <aaamargml@gmail.com>
+
+This file is part of FloppaOS.
+
+FloppaOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+FloppaOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with FloppaOS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "flopmath.h"
 
 // Factorial function for Taylor series
