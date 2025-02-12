bde7fdb726b843fdc9d9175c7f7adeb40bb5d403
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2025-02-12 08:40:21 -0800

    add error logging function

--- lib/logging.h
+++ lib/logging.h
@@ -6,4 +6,5 @@
 void log_address(const char *msg, uintptr_t addr) ;
 void log_uint(const char* prefix, uint32_t value) ;
 void log_step(const char *message, uint8_t color) ;
+void log_error(const char *message, uint8_t color) ;
 #endif
