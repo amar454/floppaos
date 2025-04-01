319dc82379412d21f65e6fdff0e92c2b89fe7003
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2025-04-01 11:15:23 -0700

    Add freeing from free list in alloc.c

--- mem/alloc.c
+++ mem/alloc.c
@@ -5,16 +5,22 @@
 #include "../lib/logging.h"
 #include "../kernel.h"
 #include "../drivers/vga/vgahandler.h"
+
 #define ALIGN_UP(x, align) (((x) + ((align)-1)) & ~((align)-1))
+#define PMM_RETURN_THRESHOLD (8 * PAGE_SIZE) // Return to PMM if free block is this big
+
 int kernel_heap_size = 0;
+
 struct alloc_mem_block {
     struct alloc_mem_block *next;
     size_t size;
 };
+
 struct free_list {
     struct free_list *next;
     size_t size;
 };
+
 struct kernel_region {
     struct kernel_region *next;
     uintptr_t start;
@@ -23,14 +29,27 @@ struct kernel_region {
 } kernel_regions;
 
 static struct free_list *free_blocks = NULL;
+static int heap_initialized = 0;
+static struct alloc_mem_block *first_block = NULL;
 
+// Add block to free list in sorted order
 void add_to_free_list(void *ptr, size_t size) {
     struct free_list *block = (struct free_list *)ptr;
     block->size = size;
-    block->next = free_blocks;
-    free_blocks = block;
+
+    struct free_list **prev = &free_blocks;
+    struct free_list *current = free_blocks;
+
+    while (current && (uintptr_t)current < (uintptr_t)block) {
+        prev = &current->next;
+        current = current->next;
+    }
+
+    block->next = current;
+    *prev = block;
 }
 
+// Get the first sufficiently large block from free list
 void *get_from_free_list(size_t size) {
     struct free_list **prev = &free_blocks;
     struct free_list *current = free_blocks;
@@ -46,46 +65,84 @@ void *get_from_free_list(size_t size) {
     return NULL;
 }
 
+// Remove block from free list
+void remove_from_free_list(struct free_list *block) {
+    struct free_list **prev = &free_blocks;
+    struct free_list *current = free_blocks;
+
+    while (current) {
+        if (current == block) {
+            *prev = current->next;
+            return;
+        }
+        prev = &current->next;
+        current = current->next;
+    }
+}
+
+// Merge adjacent free blocks
+void coalesce_free_blocks() {
+    struct free_list *current = free_blocks;
+    while (current && current->next) {
+        if ((uintptr_t)current + current->size == (uintptr_t)current->next) {
+            current->size += current->next->size;
+            current->next = current->next->next;
+        } else {
+            current = current->next;
+        }
+    }
+}
+
+// Return large free blocks to PMM
+void return_large_free_blocks() {
+    struct free_list *prev = NULL;
+    struct free_list *current = free_blocks;
+
+    while (current) {
+        if (current->size >= PMM_RETURN_THRESHOLD) {
+            if (prev)
+                prev->next = current->next;
+            else
+                free_blocks = current->next;
+
+            pmm_free_pages((void *)current, current->size / PAGE_SIZE);
+        } else {
+            prev = current;
+        }
+        current = prev ? prev->next : free_blocks;
+    }
+}
+
 void add_memory_block(uintptr_t start, size_t size, int is_free) {
     struct alloc_mem_block *block = (struct alloc_mem_block *)start;
     block->size = size;
     block->next = NULL;
 
     if (is_free) {
-
         block->size |= 1;
         add_to_free_list((void *)start, size);
     } else {
-
         block->size &= ~1;
     }
-} 
-
-
-static int heap_initialized = 0;
-static struct alloc_mem_block *first_block = NULL;
+}
 
 void init_kernel_heap(void) {
     log_step("Initializing kernel heap...\n", YELLOW);
     
     size_t total_memory = pmm_get_memory_size();
-    
     if (total_memory == 0) {
         log_step("init_kernel_heap: PMM not initialized or no memory available!\n", RED);
         PANIC_PMM_NOT_INITIALIZED((uintptr_t)first_block);
         return;
     }
-    
 
     kernel_heap_size = (total_memory * HEAP_PERCENTAGE) / 100;
     kernel_heap_size = ALIGN_UP(kernel_heap_size, PAGE_SIZE);
-    
     if (kernel_heap_size < MIN_HEAP_SIZE) {
         kernel_heap_size = ALIGN_UP(MIN_HEAP_SIZE, PAGE_SIZE);
     } else if (kernel_heap_size > MAX_HEAP_SIZE) {
         kernel_heap_size = ALIGN_UP(MAX_HEAP_SIZE, PAGE_SIZE);
     }
-    
 
     first_block = (struct alloc_mem_block *)KERNEL_HEAP_START;
     add_memory_block(KERNEL_HEAP_START, kernel_heap_size, 1);
@@ -94,14 +151,7 @@ void init_kernel_heap(void) {
     kernel_regions.end = (uintptr_t)(KERNEL_HEAP_START + kernel_heap_size);
     kernel_regions.next = NULL;
 
-
-    log_step("Kernel heap and region initialized successfully\n", GREEN);
-    log_address("Heap start address: ", KERNEL_HEAP_START);
-    log_address("Heap end address: ", KERNEL_HEAP_START + kernel_heap_size);
-    log_uint("Heap size (KB): ", kernel_heap_size / 1024);
-    log_uint("Heap size (MB): ", kernel_heap_size / (1024 * 1024));
-    log_uint("Percentage of total memory: ", HEAP_PERCENTAGE);
-    
+    log_step("Kernel heap initialized successfully\n", GREEN);
     heap_initialized = 1;
 }
 
@@ -112,6 +162,7 @@ void *kmalloc(size_t size) {
         void *ptr = slab_alloc(size);
         if (ptr) return ptr;
     }
+
     void *ptr = get_from_free_list(size);
     if (ptr) {
         struct alloc_mem_block *block = (struct alloc_mem_block *)ptr;
@@ -119,15 +170,12 @@ void *kmalloc(size_t size) {
         return (void *)(block + 1);
     }
 
-
     size_t pages = (ALIGN_UP(size + sizeof(struct alloc_mem_block), SLAB_PAGE_SIZE) / SLAB_PAGE_SIZE);
-
     ptr = pmm_alloc_pages(0, pages);
     if (!ptr) {
         log_step("kmalloc: failed to allocate large block", size);
         return NULL;
     }
-
 
     add_memory_block((uintptr_t)ptr, pages * SLAB_PAGE_SIZE, 0);
     return (void *)((struct alloc_mem_block *)ptr + 1);
@@ -143,21 +191,10 @@ void kfree(void *ptr, size_t size) {
 
     struct alloc_mem_block *block = (struct alloc_mem_block *)ptr - 1;
     add_to_free_list(block, block->size);
-    block->size |= 1; // Mark as free
+    block->size |= 1;
 
-    // Coalesce adjacent free blocks
-    struct free_list *current = free_blocks;
-    struct free_list *prev = NULL;
-    
-    while (current && current->next) {
-        if ((uintptr_t)current + current->size == (uintptr_t)current->next) {
-            current->size += current->next->size;
-            current->next = current->next->next;
-        } else {
-            prev = current;
-            current = current->next;
-        }
-    }
+    coalesce_free_blocks();
+    return_large_free_blocks();
 }
 
 void *kcalloc(uint32_t num, size_t size) {
