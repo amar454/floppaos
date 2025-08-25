1dea92a5bdce562047b6c81f5e40255f9c9b7ed9
Amar Djulovic <72467930+amar454@users.noreply.github.com>
2025-08-24 17:14:41 -0700

    Gdt: update gdt.h for static gdt

--- mem/gdt.h
+++ mem/gdt.h
@@ -3,22 +3,11 @@
 
 #include <stdint.h>
 
-typedef struct {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t granularity;
-    uint8_t base_high;
-} __attribute__((packed)) GDTEntry;
-
-typedef struct {
+typedef struct __attribute__((packed)) {
     uint16_t limit;
     uint32_t base;
-} __attribute__((packed)) GDTPointer;
+} gdtr_t;
 
-void gdt_flush(uint32_t gdt_ptr_address);
-void set_gdt_entry(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity);
-void init_gdt();
+void gdt_init(void);
 
 #endif
